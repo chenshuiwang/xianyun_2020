aafe68fd1a60f02fd27f20512d44d2a9f29bff71
chen <3160495815@qq.com>
2020-02-07 18:29:45 +0800

    头部和底部组件完成

--- components/header.vue
+++ components/header.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>头部</div>
+  <div class="container">
+    <div class="header">
+      <el-row type="flex" justify="space-between" align="middle">
+        <div class="logo">
+          <nuxt-link to="/">
+            <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+          </nuxt-link>
+        </div>
+        <el-row class="cate" type="flex">
+          <nuxt-link to="/">首页</nuxt-link>
+          <nuxt-link to="/post">旅游攻略</nuxt-link>
+          <nuxt-link to="/hotel">酒店</nuxt-link>
+          <nuxt-link to="/air">国内机票</nuxt-link>
+        </el-row>
+        <div class="login">
+          <nuxt-link to="/user/login">登录/注册</nuxt-link>
+        </div>
+      </el-row>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+}
+.header {
+  height: 60px;
+  line-height: 60px;
+  .logo {
+    width: 156px;
+    img {
+      width: 100%;
+      display: block;
+    }
+  }
+  .cate {
+    flex: 1;
+    margin: 0 20px;
+    a {
+      display: block;
+      padding: 0 20px;
+      box-sizing: border-box;
+      height: 60px;
+      &:hover {
+        border-bottom: 5px #409eff solid;
+        color: #409eff;
+      }
+    }
+    .nuxt-link-active {
+      background-color: #409eff;
+      color: #fff !important;
+    }
+  }
+  .login {
+    &:hover {
+      color: #409eff;
+      text-decoration: underline;
+    }
+  }
+}
 </style>
