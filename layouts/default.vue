ee9f132162c82a407aaf21eb6f96fc897d83e590
chen <3160495815@qq.com>
2020-02-07 13:45:45 +0800

    项目初始化

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Header></Header>
+    <nuxt />
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from '../components/header'
+import Footer from '../components/footer'
+export default {
+  components:{
+    Header,Footer
+  },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+<style lang='less'>
+*{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+a{
+  color: inherit;
+}
+em,i{
+  font-style: normal;
+}
+
+</style>
